753de270704bf9ddf101528f22eb0ccde16f009d
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2024-09-04 10:53:03 -0500

    Clean up tailwind on main container
    Main now mimics sizing convention of footer

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <Header/>
 
-<main class="grid lg:grid-cols-12 gap-4 md:gap-6 lg:mx-auto md:mx-6 max-w-[67.5rem] grid-cols-8 px-4 md:px-0">
+<main class="px-4 lg:px-20 pt-8">
   <h1 class="font-editorial-news-heading-02">{message}</h1>
 </main>
 
