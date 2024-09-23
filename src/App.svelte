9510433beba92d26714de0e5c9c65458defd64be
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2024-09-23 11:27:13 -0500

    Remove article furniture, seo/tracking
    These components are still in the app, but no longer included in the default App component now that we have a start to article deployment via iframe

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,12 @@
 <script>
-  import seo from "./config/seo.json";
-
-  import Header from "./components/page/Header.svelte";
-  import Footer from "./components/page/Footer.svelte";
-
-  import GaTrackingHead from "./components/seo/GATracking.svelte";
-  import Meta from "./components/seo/Meta.svelte";
-
 
   let message = "Hello world!"
 
 </script>
 
-<Meta
-{seo}
-/>
-
-<GaTrackingHead
-{seo}
-/>
-
-
-<Header/>
-
 <main class="px-4 lg:px-20 pt-8">
   <h1 class="font-editorial-news-heading-02">{message}</h1>
 </main>
-
-<Footer/>
 
 <style>
 
