2af2cc11e4c30c20413a41abd7ba8d0b8e37a839
Tom Nehil <tom.nehil@gmail.com>
2024-11-27 10:28:57 -0600

    Upgrades to Svelte 5

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import "./styles/tailwind/utility.css"
 import "./styles/tailwind/typography.css"
 import "./styles/tailwind/tailwind.css"
 
+import { mount } from "svelte";
 import App from './App.svelte'
 
 document.getElementById('proj-container').innerHTML = ""
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('proj-container')
 })
 
