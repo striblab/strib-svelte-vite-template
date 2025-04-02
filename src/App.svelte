b4a17712134b693478c52707f227b02150597e53
Jake Steinberg <jakesteinberg96@gmail.com>
2025-04-02 17:09:28 -0500

    latest changes

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,33 @@
   let scrollIndex = 0;
 </script>
 
-<main class="px-4 lg:px-20 pt-8">
-  <!-- Pass the scrollIndex to the map -->
-  <Map {scrollIndex} />
-  
-  <!-- The Scrolly component will update scrollIndex based on the currently visible slide -->
+<main class="relative" style="height: 400vh;">
   <Scrolly bind:value={scrollIndex}>
-    <div class="py-10">
-      <h2 class="text-2xl font-bold">Slide 1</h2>
-      <p>This text corresponds to dataset A.</p>
-    </div>
-    <div class="py-10">
-      <h2 class="text-2xl font-bold">Slide 2</h2>
-      <p>This text corresponds to dataset B.</p>
-    </div>
-    <div class="py-10">
-      <h2 class="text-2xl font-bold">Slide 3</h2>
-      <p>This text corresponds to dataset C.</p>
-    </div>
+    <section class="h-screen flex flex-col justify-center items-center step">
+      <div class="z-10 font-editorial-body-reg-01 bg-white p-4 md:p-7 h-min rounded-lg shadow-lg w-full max-w-xl mx-auto">
+        <p>Before Europeans arrived in Minnesota, wetlands filled the state’s prairies.</p>
+      </div>
+    </section>
+
+    <section class="h-screen flex flex-col justify-center items-center step">
+      <div class="z-10 font-editorial-body-reg-01 bg-white p-4 md:p-7 h-min rounded-lg shadow-lg w-full max-w-xl mx-auto">
+        <p>Today, only 5% of those wet prairies remain.</p>
+      </div>
+    </section>
+
+    <section class="h-screen flex flex-col justify-center items-center step">
+      <div class="z-10 font-editorial-body-reg-01 bg-white p-4 md:p-7 h-min rounded-lg shadow-lg w-full max-w-xl mx-auto">
+        <p>They were systemically drained by ditches and tile drains, an underground system of plumbing that keeps today’s farm fields from becoming wetlands again.</p>
+      </div>
+    </section>
+
+    <section class="h-screen flex flex-col justify-center items-center step">
+      <div class="z-10 font-editorial-body-reg-01 bg-white p-4 md:p-7 h-min rounded-lg shadow-lg w-full max-w-xl mx-auto">
+        <p>Duck hunting groups have been instrumental in bringing the wet prairies back. Ducks Unlimited has identified over 1.7 million acres of former wet prairies that could still be restored.</p>
+      </div>
+    </section>
   </Scrolly>
+  <div class="fixed top-0 w-full h-screen z-0">
+    <Map {scrollIndex} />
+  </div>
 </main>
--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,6 +1,11 @@
 <script>
-    import { onMount } from 'svelte';
-    import maplibregl from 'maplibre-gl';
+  import { onMount } from 'svelte';
+  import maplibregl from 'maplibre-gl';
+  import currentWetlands from '../data/current_wetlands.json';
+  import formerWetlands from '../data/former_wetlands.json';
+  import tiles from '../data/tiles.json';
+  import restorableWetlands from '../data/restorable_wetlands.json';
+  import basemap from '../data/strib-basemap.json';
   
   // Accept the scroll index as a prop from the parent
   export let scrollIndex = 0;
@@ -11,15 +16,95 @@
   onMount(() => {
     map = new maplibregl.Map({
       container: mapContainer,
-      style: 'https://api.maptiler.com/maps/landscape/style.json?key=vEtG6YUag2WpDIiNOgGm',
-      center: [-94, 47],
-      zoom: 6,
+      style: basemap,
+      center: [-94, 46.5],
+      zoom: 6.2,
       interactive: false
     });
+    map.on('load', () => {
+      map.addSource('formerWetlands', {
+        type: 'geojson',
+        data: formerWetlands
+      });
+      map.addLayer({
+        id: 'former_wetlands_layer',
+        type: 'fill',
+        source: 'formerWetlands',
+        paint: {
+          'fill-color': '#B6E32B',
+          'fill-opacity': 1
+        }
+      });
+      map.addSource('currentWetlands', {
+        type: 'geojson',
+        data: currentWetlands
+      });
+      map.addLayer({
+        id: 'current_wetlands_layer',
+        type: 'fill',
+        source: 'currentWetlands',
+        paint: {
+          'fill-color': '#B6E32B',
+          'fill-opacity': 1
+        }
+      });
+      map.addSource('restorableWetlands', {
+        type: 'geojson',
+        data: restorableWetlands
+      });
+      map.addLayer({
+        id: 'restorable_wetlands_layer',
+        type: 'fill',
+        source: 'restorableWetlands',
+        paint: {
+          'fill-color': '#9D86C6',
+          'fill-opacity': 1
+        }
+      });
+      map.addSource('tiles', {
+        type: 'geojson',
+        data: tiles
+      });
+      map.addLayer({
+        id: 'tiles_layer',
+        type: 'fill',
+        source: 'tiles',
+        paint: {
+          'fill-color': '#EA8B8B',
+          'fill-opacity': 1
+        }
+      });
+      map.addSource('formerWetlands2', {
+        type: 'geojson',
+        data: formerWetlands
+      });
+      map.addLayer({
+        id: 'former_wetlands_layer_2',
+        type: 'fill',
+        source: 'formerWetlands2',
+        paint: {
+          'fill-color': '#969696',
+          'fill-opacity': 0.3,
+          'fill-outline-color': '#0D0D0D'
+        }
+      });
+    map.setLayoutProperty('former_wetlands_layer', 'visibility', scrollIndex === 0 ? 'visible' : 'none');
+    map.setLayoutProperty('current_wetlands_layer', 'visibility', scrollIndex === 1 ? 'visible' : 'none');
+    map.setLayoutProperty('tiles_layer', 'visibility', scrollIndex === 2 ? 'visible' : 'none');
+    map.setLayoutProperty('former_wetlands_layer_2', 'visibility', scrollIndex === 2 ? 'visible' : 'none');
+    map.setLayoutProperty('restorable_wetlands_layer', 'visibility', scrollIndex === 3 ? 'visible' : 'none');
+    
+    })
   });
   
-
+  $: if (map && map.isStyleLoaded()) {
+    map.setLayoutProperty('former_wetlands_layer', 'visibility', scrollIndex === 0 ? 'visible' : 'none');
+    map.setLayoutProperty('current_wetlands_layer', 'visibility', scrollIndex === 1 ? 'visible' : 'none');
+    map.setLayoutProperty('tiles_layer', 'visibility', scrollIndex === 2 ? 'visible' : 'none');
+    map.setLayoutProperty('former_wetlands_layer_2', 'visibility', scrollIndex === 2 ? 'visible' : 'none');
+    map.setLayoutProperty('restorable_wetlands_layer', 'visibility', scrollIndex === 3 ? 'visible' : 'none');
+  }
 
 </script>
 
-<div bind:this={mapContainer} class="w-full h-screen"></div>
+<div bind:this={mapContainer} class="w-full h-screen z-0"></div>
