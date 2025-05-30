f5fe62e63f97c8c684371f18242862a0deedeb02
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2025-05-30 09:48:33 -0500

    Update html

--- src/components/Shared/_ImageCaption.svelte
+++ src/components/Shared/_ImageCaption.svelte
@@ -2,6 +2,6 @@
   let { children } = $props();
 </script>
 
-<div class="font-utility-meta-reg-02 text-text-secondary">
+<p class="font-utility-meta-reg-02 text-text-secondary">
   {@render children()}
-</div>
+</p>
