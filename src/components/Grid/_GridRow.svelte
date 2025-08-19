1e5c9c0dbb422a6ac8e08482aec0950a1fbe02d1
Jake Steinberg <jakesteinberg96@gmail.com>
2025-08-19 16:18:53 -0500

    update gridrow

--- src/components/Grid/_GridRow.svelte
+++ src/components/Grid/_GridRow.svelte
@@ -2,15 +2,16 @@
 @component
 ### GridRow component
 Renders a div that spans a configurable number of columns when used within a parent Grid component.  
-Wrap this component around additional markup to control that markup's width and position within its grid layout.
+Wrap this component around additional markup to control that markup's width and position within a parent's grid layout.
 
 #### Optional properties
-- variant: "default" | "inline";
+- variant: "default" | "inline" | "fullBleed";
 - additionalClasses: string;
 
 #### Variants
 - default: GridRow spans the full width of its parent grid. This is the default behavior if the optional variant property is not passed in.  
 - inline: GridRow spans the full width of its parent grid on mobile, and the middle six columns of its parent grid on tablet and desktop.
+- fullBleed: GridRow spans the width of the page up to 1800px. 
 
 #### Example
 ```svelte
@@ -25,20 +26,19 @@ Wrap this component around additional markup to control that markup's width and 
 -->
 
 <script>
-  /** @type {{variant?: "default" | "inline"; additionalClasses?: string; children?: function}} */
+  /** @type {{variant?: "default" | "inline" | "fullBleed"; additionalClasses?: string; children?: function}} */
   let { variant = "default", additionalClasses = "", children } = $props();
 
-  let variantStyles = $state("");
-
-  switch (variant) {
-    case "default":
-      variantStyles = "col-span-full";
-      break;
-    case "inline":
-      variantStyles =
-        "col-span-full grid md:col-start-2 md:col-span-6 lg:col-start-4";
-      break;
-  }
+  let variantStyles = $derived.by(() => {
+    switch (variant) {
+      case "default":
+        return "col-span-full";
+      case "inline":
+        return "col-span-full grid md:col-start-2 md:col-span-6 lg:col-start-4";
+      case "fullBleed":
+        return "col-span-full w-screen relative left-1/2 -translate-x-1/2 max-w-[1800px]";
+    }
+  });
 </script>
 
 <div class="{variantStyles} {additionalClasses}">
