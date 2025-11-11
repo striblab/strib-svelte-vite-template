cc50ec8d40ebf72b6b3064615d2bade64e25e3d4
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2025-11-11 16:02:02 -0600

    Add rehydration
    The following commit checks for 10 seconds after page load whether markup needs to be rehydrated.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,39 @@
 import { hydrate } from "svelte";
 import App from "./App.svelte";
 
-const app = hydrate(App, { target: document.getElementById("proj-container") });
+let app;
+const target = document.getElementById("proj-container");
+const appMarkup = target.innerHTML;
+
+function hydrateApp() {
+  const target = document.getElementById("proj-container");
+  app = hydrate(App, { target });
+  if (target) target.setAttribute("data-svelte-hydrated", "true");
+}
+
+hydrateApp();
+
+//this replicates React's DOM removal
+// setTimeout(() => {
+//   if (target) {
+//     target.innerHTML = "";
+//     target.innerHTML = appMarkup;
+//     target.setAttribute("data-svelte-hydrated", "false");
+//   }
+//   console.log("dom nuked");
+// }, 2000);
+
+const rehydrateId = setInterval(() => {
+  const isHydrated =
+    document
+      .getElementById("proj-container")
+      .getAttribute("data-svelte-hydrated") === "true";
+
+  if (!isHydrated) hydrateApp();
+});
+
+setTimeout(() => {
+  clearInterval(rehydrateId);
+}, 10000);
 
 export default app;
