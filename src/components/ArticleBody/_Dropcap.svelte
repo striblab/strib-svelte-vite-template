5721dbd844e657c5307e245c1a7f9bb7da112dc2
Ellie Lin <ellie.ellielin@gmail.com>
2025-12-02 14:45:09 -0600

    Removed ternary, sets dropcapTypeface default font in property declaration

--- src/components/ArticleBody/_Dropcap.svelte
+++ src/components/ArticleBody/_Dropcap.svelte
@@ -3,9 +3,9 @@
 ### Dropcap component
 Creates a dropcap to be used inside a Paragraph component. 
 The typeface defaults to Publico Text Roman to match the Immersive Template Storybook {@link https://storybook-startribune-com.vercel.app/?path=/story/molecules-articletoolkit-c-a02-articlebody--immersive}, 
-otherwise pass in a different typeface om ArticleBody. 
+otherwise pass in a different typeface prop on ArticleBody. 
 Some minor alterations to the size of the dropcap and 
-top/left position might be necessary depending on the 
+top/left position might be necessary depending on both the 
 typeface and how many lines the dropcap should span.
 
 #### Example
@@ -16,13 +16,10 @@ typeface and how many lines the dropcap should span.
 
 <script>
   /** @type {{children?: function}} */
-  let { children, dropcapTypeface = "" } = $props();
+  let { children, dropcapTypeface = "font-publico-text-roman" } = $props();
 </script>
 
 <span
-  class="md:text-[75px] md:pr-2 md:-top-[0.72rem] md:left-0 md:-mb-7 text-[70px] pr-4 -top-[9px] left-1 -mb-5 float-left relative 
-  {dropcapTypeface
-        ? dropcapTypeface
-        : 'font-publico-text-roman'}"
+  class="md:text-[75px] md:pr-2 md:-top-[0.72rem] md:left-0 md:-mb-7 text-[70px] pr-4 -top-[9px] left-1 -mb-5 float-left relative {dropcapTypeface}"
   >{@render children?.()}
 </span>
