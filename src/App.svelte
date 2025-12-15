761a6d467f7b03a351eaa78eca2fe43ead760b9f
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2025-12-15 16:05:19 -0600

    Add immersiveWrapper Grid variant
    Adds a variant for the Svelte template's Grid component that replicates the present spacing of startribune-com's  immersive template's outer grid wrapper

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,26 @@
 <script>
-    import Grid from "./components/Grid/Grid.svelte";
-    import GridRow from "./components/Grid/_GridRow.svelte";
-    import ArticleBody from "./components/ArticleBody/ArticleBody.svelte";
-    import Hero from "./components/Hero/Hero.svelte";
+  import Grid from "./components/Grid/Grid.svelte";
+  import GridRow from "./components/Grid/_GridRow.svelte";
+  import ArticleBody from "./components/ArticleBody/ArticleBody.svelte";
+  import Hero from "./components/Hero/Hero.svelte";
 </script>
 
-<Grid additionalClasses="gap-y-8 md:gap-y-10 px-4">
-    <GridRow>
-        <Hero
-            sectionLabel="Section label"
-            headline="Lorem ipsum dolor sit amet consectetur adipiscing elit"
-            timestamp="Dec. 1, 1977"
-            heroImageUrl="https://arc.stimg.co/startribunemedia/4SPNT7DI36ANT2SOB5N5EJAIJU.jpg"
-            heroImageCaption="Lorem ipsum dolor"
-            heroImageAltText=""
-            dek="Lorem ipsum dolor sit amet consectetur adipiscing elit, fusce sociis at montes vitae tempor enim, venenatis tristique feugiat arcu dis ridiculus."
-            authorName="Bryan Brussee"
-            authorBioUrl="https://www.startribune.com/bryan-brussee/8455834"
-        />
-    </GridRow>
+<Grid variant="immersiveWrapper">
+  <GridRow>
+    <Hero
+      sectionLabel="Section label"
+      headline="Lorem ipsum dolor sit amet consectetur adipiscing elit"
+      timestamp="December 1, 1977"
+      heroImageUrl="https://arc.stimg.co/startribunemedia/4SPNT7DI36ANT2SOB5N5EJAIJU.jpg"
+      heroImageCaption="Lorem ipsum dolor"
+      heroImageAltText=""
+      dek="Lorem ipsum dolor sit amet consectetur adipiscing elit, fusce sociis at montes vitae tempor enim, venenatis tristique feugiat arcu dis ridiculus."
+      authorName="Bryan Brussee"
+      authorBioUrl="https://www.startribune.com/bryan-brussee/8455834"
+    />
+  </GridRow>
 
-    <GridRow>
-        <ArticleBody />
-    </GridRow>
+  <GridRow>
+    <ArticleBody />
+  </GridRow>
 </Grid>
