9cf275108a274442861e391b16d7b1ec0968fc6a
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2025-12-16 12:55:40 -0600

    Revert "Merge branch 'add-hydrate' into main"
    This reverts commit c7f91d2fecdab30c79f6ec997d7d715bc0a07866, reversing
    changes made to 4767a4c58c1126c31da092508229321166805bdd.

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import fs from "fs";
-import path from "path";
-
-const __dirname = path.resolve();
-
-const ssrDevMiddlewarePlugin = () => ({
-  name: "custom-ssr-dev-middleware",
-  configureServer(server) {
-    server.middlewares.use("/", async (req, res, next) => {
-      if (req.originalUrl !== "/") return next();
-
-      try {
-        let template = fs.readFileSync(
-          path.resolve(__dirname, "index.html"),
-          "utf-8"
-        );
-        template = await server.transformIndexHtml(req.originalUrl, template);
-        const serverEntry = await server.ssrLoadModule("/src/server.js");
-
-        const html = template.replace(
-          `<!-- SSR_CONTENT_PLACEHOLDER -->`,
-          serverEntry.render().body
-        );
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        res.end(html);
-      } catch (e) {
-        console.error("SSR Error in Vite Config:", e);
-        server.ssrFixStacktrace(e);
-        next(e);
-      }
-    });
-  },
-});
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
   base: "",
-  plugins: [svelte(), ssrDevMiddlewarePlugin()],
+  plugins: [
+    svelte(),
+    viteCompression({
+      filter: /\.(js|css|svg)$/, // Compress only these file types
+    }),
+  ],
   server: {
     open: true,
   },
