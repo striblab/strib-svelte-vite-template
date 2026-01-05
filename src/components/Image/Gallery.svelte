b26d2985fb31f6d018db19c5b65dec9654686d51
Bryan Brussee <20879219+Bryan-Brussee@users.noreply.github.com>
2026-01-05 13:31:03 -0600

    Support for mobile + tablet, captions, pad gallery
    This commit sizes gallery images as they are sized across tablet and mobile viewports in the website's immersive template, and implements captions. It also pads out short image arrays, so any number of unique images will always loop gracefully.

--- src/components/Image/Gallery.svelte
+++ src/components/Image/Gallery.svelte
@@ -12,22 +12,24 @@
 
   import IconButton from "../Button/IconButton.svelte";
 
-  //sort of weird that three is the mimnimum supported here
   let {
     imgs = [
-      "https://arc.stimg.co/startribunemedia/5K6ZGY6TU5D4TH7O22FA452H2U.jpg",
-      "https://arc.stimg.co/startribunemedia/5K6ZGY6TU5D4TH7O22FA452H2U.jpg",
-      "https://arc.stimg.co/startribunemedia/5K6ZGY6TU5D4TH7O22FA452H2U.jpg",
-      "https://arc.stimg.co/startribunemedia/5K6ZGY6TU5D4TH7O22FA452H2U.jpg",
+      {
+        img: "https://arc.stimg.co/startribunemedia/5K6ZGY6TU5D4TH7O22FA452H2U.jpg",
+        caption: "Caption 1 tk tk tk",
+      },
     ],
   } = $props();
+
+  //sort of weird, but 5 images is the fewest that can be gracefully looped
+  //hence, this function, which effectively appends on image array to itself until it can be gracefully looped
+  let paddedImgs = $derived(imgs.length < 5 ? padImgs(imgs) : imgs);
+
+  let activeIndex = $state(0);
 
   const id = $props.id();
   const nextClass = `swiper-button-next-${id}`;
   const prevClass = `swiper-button-prev-${id}`;
-
-  //TODO: Fit this to dynamic behavior defined at https://github.com/MinneapolisStarTribune/startribune-web/blob/main/src/molecules/ArticleToolkit/PhotoGallery/PhotoGallery.tsx
-  const gallerySpaceBetween = 24;
 
   const swiperParams = {
     effect: "coverflow",
@@ -47,9 +49,21 @@
       nextEl: `.${nextClass}`,
       prevEl: `.${prevClass}`,
     },
-    spaceBetween: gallerySpaceBetween,
+    spaceBetween: 8,
+    breakpoints: {
+      768: {
+        spaceBetween: 16,
+      },
+      1160: {
+        spaceBetween: 24,
+      },
+    },
     on: {
       init() {},
+      slideChange() {
+        //realIndex is appropriate for a looping gallery
+        activeIndex = this.realIndex;
+      },
     },
   };
 
@@ -57,21 +71,42 @@
     Object.assign(node, swiperParams);
     node.initialize();
   }
+
+  function padImgs(imgs) {
+    let paddedImgs = [...imgs];
+
+    while (paddedImgs.length < 5) {
+      paddedImgs.push(...paddedImgs);
+    }
+
+    return paddedImgs;
+  }
 </script>
 
-<div class="">
+<div class="pt-5">
   <div class="mb-2">
     <swiper-container aria-live="polite" init="false" use:setupSwiper>
-      {#each imgs as img}
-        <swiper-slide class="max-w-[1080px]">
-          <img src={img} alt="" />
+      {#each paddedImgs as img}
+        {console.log(img)}
+        <swiper-slide
+          class="max-[389px]:w-[90%] max-w-[22.375rem] md:max-w-[33.4375rem] lg:max-w-[67.5rem]"
+        >
+          <img src={img.img} alt="" />
         </swiper-slide>
       {/each}
     </swiper-container>
   </div>
 
-  <div class="flex flex-row justify-end gap-1.5 max-w-[1080px] mx-auto">
-    <IconButton icon="/svg/chevron-left.svg" additionalClasses={prevClass} />
-    <IconButton icon="/svg/chevron-right.svg" additionalClasses={nextClass} />
+  <div
+    class="flex flex-row justify-between gap-5 max-w-[22.375rem] md:max-w-[33.4375rem] lg:max-w-[67.5rem] max-lg:px-4 mx-auto"
+  >
+    <div class=" font-utility-meta-reg-02 text-text-secondary">
+      {paddedImgs[activeIndex].caption}
+    </div>
+
+    <div class="flex flex-row justify-end gap-1.5">
+      <IconButton icon="/svg/chevron-left.svg" additionalClasses={prevClass} />
+      <IconButton icon="/svg/chevron-right.svg" additionalClasses={nextClass} />
+    </div>
   </div>
 </div>
